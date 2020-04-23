7fa53207fe7f29fdafbc0e22afd996f6e3e4ad68
sortheish <you@example.com>
2020-04-23 14:46:39 +0530

    Added recycler view

--- app/src/main/java/com/example/shopswag/controller/MainActivity.kt
+++ app/src/main/java/com/example/shopswag/controller/MainActivity.kt
@@ -2,17 +2,27 @@ package com.example.shopswag.controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.size
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.shopswag.R
 import com.example.shopswag.adapter.CatergoryAdapter
+import com.example.shopswag.adapter.CatergoryRecyclerAdapter
 import com.example.shopswag.services.DataService
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    lateinit var categoryAdapter : CatergoryAdapter
+    lateinit var categoryAdapter : CatergoryRecyclerAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        categoryListView.adapter = CatergoryAdapter(this,DataService.categories)
+        val adapter = CatergoryRecyclerAdapter(this,DataService.categories)
+        categoryListView.adapter = adapter
+
+        val layoutManger = LinearLayoutManager(this)
+        categoryListView.layoutManager = layoutManger
+        categoryListView.setHasFixedSize(true)
+
     }
 }
